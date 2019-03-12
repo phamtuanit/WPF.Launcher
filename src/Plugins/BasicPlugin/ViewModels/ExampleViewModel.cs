fef19ff53b20ad411c2d9d8602f20b28914c8707
tuanpham <tuanpham@gcs-vn.com>
2019-03-12 18:17:57 +0800

    Add back BasicPlugin

---
+++ src/Plugins/BasicPlugin/ViewModels/ExampleViewModel.cs
@@ -0,0 +1,26 @@
+﻿using MEF.Launcher.Platform.Screen;
+using System.ComponentModel.Composition;
+
+namespace BasicPlugin.ViewModels
+{
+    [Export]
+    public class ExampleViewModel : ScreenBase
+    {
+        /// <summary>
+        /// Gets or Sets the Display Name
+        /// </summary>
+        public override string DisplayName { get; set; }
+
+        #region Command
+
+        /// <summary>
+        /// Shows the form command.
+        /// </summary>
+        public void ShowFormCmd()
+        {
+            this.DialogManager.ShowBusyIndicator();
+        }
+
+        #endregion
+    }
+}
