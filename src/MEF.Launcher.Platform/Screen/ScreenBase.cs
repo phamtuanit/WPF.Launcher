4b764dcd59f61e9383a215e8f11fed6986437c0e
tuanpham <tuanpham@gcs-vn.com>
2019-03-20 10:12:18 +0700

    Upgraded Fody nuget

--- src/MEF.Launcher.Platform/Screen/ScreenBase.cs
+++ src/MEF.Launcher.Platform/Screen/ScreenBase.cs
@@ -1,11 +1,17 @@
 ﻿using log4net;
 using MEF.Launcher.Contract;
 using MEF.Launcher.Contract.IoC;
+using System.ComponentModel;
 
 namespace MEF.Launcher.Platform.Screen
 {
-    public class ScreenBase : Caliburn.Micro.Screen, IScreen
+    public class ScreenBase : Caliburn.Micro.Screen, IScreen, INotifyPropertyChanged
     {
+        /// <summary>
+        /// Implement INotifyPropertyChanged
+        /// </summary>
+        public override event PropertyChangedEventHandler PropertyChanged;
+
         /// <summary>
         /// The logger
         /// </summary>
