3f82ed6840632584c2c9730db16c8ed970f8a5bd
tuanpham <tuanppham@gcs-vn.com>
2019-05-19 10:17:43 +0700

    Add IoT devices plugin

---
+++ src/Plugins/Devices/CustomAttribute/PluggablePartExportAttribute.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPlugin.CustomAttribute
+{
+    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
+    [MetadataAttribute]
+    public class PluggablePartExportAttribute : ExportAttribute, IPluggablePartMetadata
+    {
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PluggablePartExportAttribute"/> class.
+        /// </summary>
+        public PluggablePartExportAttribute() : base()
+        {
+
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PluggablePartExportAttribute"/> class.
+        /// </summary>
+        /// <param name="contractType">A type from which to derive the contract name that is used to export the type or member marked with this attribute, or <see langword="null" /> to use the default contract name.</param>
+        public PluggablePartExportAttribute(Type contractType) : base(contractType)
+        {
+
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PluggablePartExportAttribute"/> class.
+        /// </summary>
+        /// <param name="contractName">The contract name that is used to export the type or member marked with this attribute, or <see langword="null" /> or an empty string ("") to use the default contract name.</param>
+        public PluggablePartExportAttribute(string contractName) : base(contractName)
+        {
+            this.Name = contractName;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PluggablePartExportAttribute"/> class.
+        /// </summary>
+        /// <param name="contractName">The contract name that is used to export the type or member marked with this attribute, or <see langword="null" /> or an empty string ("") to use the default contract name.</param>
+        /// <param name="contractType">The type to export.</param>
+        public PluggablePartExportAttribute(string contractName, Type contractType) : base(contractName, contractType)
+        {
+            this.Name = contractName;
+        }
+    }
+}
---
+++ src/Plugins/Devices/Models/IoT/Channel.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using PropertyChanged;
+
+namespace BasicPlugin.Models.IoT
+{
+    [ImplementPropertyChanged]
+    public class Channel : MarshalByRefObject, ICloneable
+    {
+        public event Action<Channel, Channel> DataChangedEvent;
+
+        internal string Type { get; set; }
+
+        public int Ch { get; set; } = 0;
+
+        public int? Md { get; set; } = 0;
+
+        [AlsoNotifyFor("Status")]
+        public int Val { get; set; } = 0;
+
+        public int? Stat { get; set; } = 0;
+
+        public int? Cnting { get; set; } = 1;
+
+        public int? OvLch { get; set; } = 0;
+
+        [XmlIgnore]
+        public bool Status
+        {
+            get
+            {
+                return this.Val == 1;
+            }
+            set
+            {
+                this.Val = value ? 1 : 0;
+            }
+        }
+
+        public Channel()
+        {
+
+        }
+
+        public Channel(string type) : this()
+        {
+            this.Type = type;
+        }
+
+        public Channel(int id, string type) : this()
+        {
+            this.Ch = id;
+            this.Type = type;
+        }
+
+        internal void Update(Channel newData)
+        {
+            var oldInfo = this.Clone() as Channel;
+            this.Val = newData.Val;
+
+            if (newData.Md != null)
+            {
+                this.Md = newData.Md;
+            }
+            if (newData.Cnting != null)
+            {
+                this.Cnting = newData.Cnting;
+            }
+            if (newData.Stat != null)
+            {
+                this.Stat = newData.Stat;
+            }
+            if (newData.OvLch != null)
+            {
+                this.OvLch = newData.OvLch;
+            }
+
+            this.DataChangedEvent?.Invoke(oldInfo, this);
+        }
+
+        public object Clone()
+        {
+            return (Channel) this.MemberwiseClone();
+        }
+
+        public override string ToString()
+        {
+            return $"[{this.Type}] [\"Ch\" : {this.Ch}, \"Val\" : {this.Val}, \"Md\" : {this.Md}, \"Stat\" : {this.Stat}, \"Cnting\" : {this.Cnting}, \"OvLch\" : {this.OvLch}]";
+        }
+
+        public override object InitializeLifetimeService()
+        {
+            return null;
+        }
+    }
+}
